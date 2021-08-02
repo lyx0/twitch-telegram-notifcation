c6935d6173fa16be524c2d69bd667661aff246f0
lyx0 <stefan@parfuss.me>
2021-08-02 18:31:00 +0200

    init commit

---
+++ clients/twitch.js
@@ -0,0 +1,49 @@
+const {
+    ChatClient,
+    AlternateMessageModifier,
+    UserStateTracker,
+    SlowModeRateLimiter,
+} = require("dank-twitch-irc");
+const chalk = require("chalk");
+const log = console.log;
+
+require("dotenv").config();
+
+const client = new ChatClient({
+    username: process.env.TWITCH_USER,
+    password: process.env.TWITCH_OAUTH,
+    rateLimits: "default",
+    ignoreUnhandledPromiseRejections: true,
+});
+
+// Allows the bot to send the same message
+client.use(new AlternateMessageModifier(client));
+
+// Allows the bot to know if they're mod/vip/sub or pleb
+client.use(new UserStateTracker(client));
+
+// Rate Limiter so we don't get globaled
+client.use(new SlowModeRateLimiter(client, 2));
+
+client.initialize = async () => {
+    await client.join("nouryqt");
+    await client.connect();
+};
+
+client.on("ready", () => {
+    log(chalk.bold.green("[TWITCH] Successfully connected to chat"));
+
+    client.say("nouryqt", "FeelsDankMan");
+});
+
+client.on("close", (error) => {
+    if (error != null) {
+        log(chalk.bold.red("Client closed due to error", error));
+    }
+});
+
+client.on("PRIVMSG", (msg) => {
+    log(chalk.white(`[TWITCH] [#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`));
+});
+
+module.exports = client;
---
+++ main.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+const log = console.log;
+
+twitchClient = require("./clients/twitch");
+
+twitchClient.initialize();

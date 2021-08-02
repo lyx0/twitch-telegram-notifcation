4452c0317a2a014d5b1be0a755077269984a7c35
lyx0 <stefan@parfuss.me>
2021-08-02 18:56:49 +0200

    change from joinAll to normal join

--- clients/twitch.js
+++ clients/twitch.js
@@ -25,7 +25,11 @@ client.use(new UserStateTracker(client));
 client.use(new SlowModeRateLimiter(client, 2));
 
 client.initialize = async () => {
-    await client.joinAll("nouryqt", "noemience", "elajjaz", "pajlada", "zneix");
+    await client.join("nouryqt");
+    await client.join("noemience");
+    await client.join("elajjaz");
+    await client.join("pajlada");
+    await client.join("zneix");
     await client.connect();
 };
 

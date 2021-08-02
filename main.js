6501a3fc1629ebcfbaf364dcdbbabb4ca3f44869
lyx0 <stefan@parfuss.me>
2021-08-02 18:54:40 +0200

    connect telegram and twitch

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ const log = console.log;
 const twitchClient = require("./clients/twitch");
 const telegramClient = require("./clients/telegram");
 
+const telegramChatId = process.env.TELEGRAM_CHAT_ID;
+
+const nameToPing = "noury";
+
 twitchClient.initialize();
+
+twitchClient.on("PRIVMSG", (msg) => {
+    if (!msg.messageText.toLowerCase().includes(nameToPing)) {
+        return;
+    } else {
+        const messageStr = `#[${msg.channelName}] ${msg.senderUsername}: ${msg.messageText}`;
+        telegramClient.sendMessage(telegramChatId, messageStr);
+    }
+});
 
 // telegramClient.listen(8080, function listen() {
 //     console.log("Listening on port 8080");
